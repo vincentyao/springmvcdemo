d79d62321cce5bfe17ccccdbf30668ea0294338f
vincent.yao <75587880@qq.com>
2017-06-26 15:32:33 +0800

    modified mainController

--- src/main/java/com/onion/controller/MainController.java
+++ src/main/java/com/onion/controller/MainController.java
@@ -6,6 +6,7 @@ import com.onion.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -24,13 +25,37 @@ public class MainController {
     }
 
     @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
-    public String getUsers(ModelMap modelMap){
+    public String getUsers(ModelMap modelMap) {
         List<UserEntity> userList = userRepository.findAll();
 
 
-        modelMap.addAttribute("userList",userList);
+        modelMap.addAttribute("userList", userList);
         return "admin/users";
     }
 
+    // get请求，访问添加用户 页面
+    @RequestMapping(value = "/admin/users/add", method = RequestMethod.GET)
+    public String addUser() {
+        // 转到 admin/addUser.jsp页面
+        return "admin/addUser";
+    }
+
+
+    // post请求，处理添加用户请求，并重定向到用户管理页面
+    @RequestMapping(value = "/admin/users/addPer", method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity) {
+        // 注意此处，post请求传递过来的是一个UserEntity对象，里面包含了该用户的信息
+        // 通过@ModelAttribute()注解可以获取传递过来的'user'，并创建这个对象
+
+        // 数据库中添加一个用户，该步暂时不会刷新缓存
+        //userRepository.save(userEntity);
+
+        // 数据库中添加一个用户，并立即刷新缓存
+        userRepository.saveAndFlush(userEntity);
+
+        // 重定向到用户管理页面，方法为 redirect:url
+        return "redirect:/admin/users";
+
+    }
 }
 
