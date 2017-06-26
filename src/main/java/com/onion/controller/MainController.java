7f175f189ae2e6e7f214a5f3266fef957dc9a0f3
vincent.yao <75587880@qq.com>
2017-06-26 10:30:44 +0800

    modified class

--- src/main/java/com/onion/controller/MainController.java
+++ src/main/java/com/onion/controller/MainController.java
@@ -3,7 +3,9 @@ package com.onion.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import com.onion.model.UserEntity;
+import com.onion.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowire;
 
 /**
  * Created by dzkan on 2016/3/8.
