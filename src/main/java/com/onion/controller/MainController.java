959dab4b86528abdcd14dd723be94b5aeee44c7b
vincent.yao <75587880@qq.com>
2017-06-26 18:34:41 +0800

    modified proje

--- src/main/java/com/onion/controller/MainController.java
+++ src/main/java/com/onion/controller/MainController.java
@@ -72,5 +72,28 @@ public class MainController {
         modelMap.addAttribute("user", userEntity);
         return "admin/details";
     }
+
+    // 更新用户信息 页面
+    @RequestMapping(value = "/admin/users/update/{id}", method = RequestMethod.GET)
+    public String updateUser(@PathVariable("id") Integer userId, ModelMap modelMap) {
+
+        // 找到userId所表示的用户
+        UserEntity userEntity = userRepository.findOne(userId);
+
+        // 传递给请求页面
+        modelMap.addAttribute("user", userEntity);
+        return "admin/updateUser";
+    }
+
+    // 更新用户信息 操作
+    @RequestMapping(value = "/admin/users/updateP", method = RequestMethod.POST)
+    public String updateUserPost(@ModelAttribute("userP") UserEntity user) {
+
+        // 更新用户信息
+        userRepository.updateUser(user.getNickname(), user.getFirstName(),
+                user.getLastName(), user.getPassword(), user.getId());
+        userRepository.flush(); // 刷新缓冲区
+        return "redirect:/admin/users";
+    }
 }
 
--- src/main/java/com/onion/repository/UserRepository.java
+++ src/main/java/com/onion/repository/UserRepository.java
@@ -18,7 +18,7 @@ public interface UserRepository extends JpaRepository<UserEntity,Integer>{
     @Modifying
     @Transactional
 
-    @Query("update UserEntity us set us.nickname=:qNickname, us.firstName=:qFirstName, us.lastName=:qLastName, us.password=:qPassword where us.id=:qId")
+    @Query("update UserEntity us set us.id=:qId,us.nickname=:qNickname, us.firstName=:qFirstName, us.lastName=:qLastName, us.password=:qPassword where us.id=:qId")
     public void updateUser(@Param("qNickname") String nickname, @Param("qFirstName") String firstName,
                            @Param("qLastName") String qLastName, @Param("qPassword") String password, @Param("qId") Integer id);
 }
