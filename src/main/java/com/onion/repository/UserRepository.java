0a9eac4c7ec25960bec194e4f572a7552f60e0ad
vincent.yao <75587880@qq.com>
2017-06-26 18:17:18 +0800

    modified proje

--- src/main/java/com/onion/repository/UserRepository.java
+++ src/main/java/com/onion/repository/UserRepository.java
@@ -1,10 +1,25 @@
 package com.onion.repository;
+
 import com.onion.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 /**
  * Created by 161116 on 2017/6/26.
  */
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer>{
+    @Modifying
+    @Transactional
+
+    @Query("update UserEntity us set us.nickname=:qNickname, us.firstName=:qFirstName, us.lastName=:qLastName, us.password=:qPassword where us.id=:qId")
+    public void updateUser(@Param("qNickname") String nickname, @Param("qFirstName") String firstName,
+                           @Param("qLastName") String qLastName, @Param("qPassword") String password, @Param("qId") Integer id);
 }
+
