2e9513dd4249bf5571fc4b1e7ad471c18ec1afee
vincent.yao <75587880@qq.com>
2017-06-26 11:21:42 +0800

    modified mainController

--- src/main/java/com/onion/controller/MainController.java
+++ src/main/java/com/onion/controller/MainController.java
@@ -1,22 +1,36 @@
 package com.onion.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.onion.model.UserEntity;
 import com.onion.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
 
 /**
  * Created by dzkan on 2016/3/8.
  */
 @Controller
 public class MainController {
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-
         return "index";
     }
+
+    @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
+    public String getUsers(ModelMap modelMap){
+        List<UserEntity> userList = userRepository.findAll();
+
+
+        modelMap.addAttribute("userList",userList);
+        return "admin/users";
+    }
+
 }
 
